c93b26705c1126007aaffaccbeb328595c750d3f
MichaelMinh <Michael@MichaelMinh.local>
2025-08-02 02:05:10 +0700

    Fixed ApiError

--- Backend/src/libs/utils.js
+++ Backend/src/libs/utils.js
@@ -1,17 +1,25 @@
 import jwt from "jsonwebtoken";
 
+export class ApiError extends Error {
+    constructor(statusCode, message) {
+        super(message);
+        this.statusCode = statusCode;
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
 export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
 
     res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-        httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
         secure: process.env.NODE_ENV !== "development",
     });
 
     return token;
 };
-
